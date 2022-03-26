879e4d7988db585b30144cd97fe1043ad9b7562a
Himanshu2468 <himanshudasingh@gmail.com>
2022-03-26 08:08:17 +0530

    first commit

---
+++ lib/pokemon.dart
@@ -0,0 +1,14 @@
+class Pokemon {
+  String image;
+  String name;
+  String weight;
+  String height;
+  String candy;
+  Pokemon({
+    required this.name,
+    required this.image,
+    required this.weight,
+    required this.height,
+    required this.candy,
+  });
+}
